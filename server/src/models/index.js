fddeae94c76e7cb9ec1da2cffbf131827102c633
Kerrin Gillis <kerrin.gillis@launchacademy.com>
2023-01-17 17:00:03 -0500

    finished code

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,4 +1,5 @@
 // include all of your models here using CommonJS requires
 const Model = require("./Model")
+const Hobby = require("./Hobby")
 
-module.exports = { Model }
+module.exports = { Model, Hobby }
--- server/src/routes/api/v1/hobbiesRouter.js
+++ server/src/routes/api/v1/hobbiesRouter.js
@@ -1,13 +1,32 @@
 import express from "express"
 
+import Hobby from "../../../models/Hobby.js"
+
 const hobbiesRouter = new express.Router()
 
 hobbiesRouter.get("/", async (req, res) => {
-
+  try {
+    const hobbies = await Hobby.query()
+    return res.status(200).json({ hobbies: hobbies })
+    // return res.status(200).json({ hobbies })
+  } catch (error) {
+    return res.status(500).json({ errors: error })
+  }
 })
 
 hobbiesRouter.post("/", async (req, res) => {
-
+  try {
+    console.log(req.body);
+    
+    // await Hobby.query().insert({ name: "", description: "" })
+    // const newHobby = await Hobby.query().insertAndFetch(req.body)
+    const hobby = await Hobby.query().insertAndFetch(req.body)
+    // console.log(newHobby);
+    return res.status(201).json({ hobby })
+    // return res.status(201).json({ hobby: newHobby })
+  } catch (error) {
+    return res.status(500).json({ errors: error })
+  }
 })
 
 export default hobbiesRouter
